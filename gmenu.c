d734d8f5e77e09acd2cb8496c6d2f718a419d1e9
Nicholas Rodrigues Lordello <n@lordello.net>
2026-05-07 17:41:57 +0200

    Custom Key Binding Configuration
    We now allow custom keybinding configurations which will cause the gmenu
    tool exit with special codes to indicate a specific keybind was used
    (instead of Return). This will allow us to, for example, show a menu for
    1Password items where M-c can be used to copy the OTP code instead of
    the password.

--- gmenu.c
+++ gmenu.c
@@ -1,13 +1,21 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
+#include <getopt.h>
 #include <gio/gio.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <string.h>
 
 #define BUS_NAME "org.gnome.Shell.Extensions.GMenu"
 #define OBJECT_PATH "/org/gnome/Shell/Extensions/GMenu"
 #define INTERFACE_NAME "org.gnome.Shell.Extensions.GMenu"
+
+#define MAX_KEYBINDINGS 16
+
+typedef struct {
+  int code;
+  const char *binding;
+} Keybinding;
 
 typedef struct {
   GMainLoop *loop;
@@ -27,15 +35,16 @@ static void on_item_selected(GDBusConnection *connection,
   (void)signal_name;
 
   State *state = user_data;
+  gint32 code;
   GVariantIter *iter = NULL;
-  g_variant_get(parameters, "(as)", &iter);
+  g_variant_get(parameters, "(ias)", &code, &iter);
   const gchar *item;
   while (g_variant_iter_loop(iter, "&s", &item)) {
     puts(item);
   }
   g_variant_iter_free(iter);
 
-  state->exit_code = EXIT_SUCCESS;
+  state->exit_code = 0x10 + code;
   g_main_loop_quit(state->loop);
 }
 
@@ -63,8 +72,7 @@ static gboolean on_stdin_ready(GIOChannel *channel, GIOCondition cond,
   gchar *line = NULL;
   gsize len = 0;
   GError *error = NULL;
-  GIOStatus status =
-      g_io_channel_read_line(channel, &line, &len, NULL, &error);
+  GIOStatus status = g_io_channel_read_line(channel, &line, &len, NULL, &error);
 
   switch (status) {
   case G_IO_STATUS_NORMAL:
@@ -90,18 +98,61 @@ static gboolean on_stdin_ready(GIOChannel *channel, GIOCondition cond,
   }
 }
 
+static gboolean parse_keybinding(char *arg, Keybinding *out) {
+  char *colon = strchr(arg, ':');
+  if (!colon || colon == arg || colon[1] == '\0') {
+    return FALSE;
+  }
+  *colon = '\0';
+  char *end;
+  long code = strtol(arg, &end, 10);
+  if (*end != '\0' || code < 0 || code >= 0x10) {
+    return FALSE;
+  }
+  out->code = (int)code;
+  out->binding = colon + 1;
+  return TRUE;
+}
+
 static void usage(FILE *out, const char *prog) {
-  fprintf(out, "usage: %s [-p prompt]\n", prog);
+  fprintf(out,
+          "usage: %s [options]\n"
+          "options:\n"
+          "  -p, --prompt PROMPT  set the menu prompt\n"
+          "      --kb CODE:KEY    bind a custom keyboard shortcut\n"
+          "  -h, --help           show this help message\n",
+          prog);
 }
 
 int main(int argc, char **argv) {
   const char *prompt = ">";
+  Keybinding keybindings[MAX_KEYBINDINGS];
+  size_t n_keybindings = 0;
+
+  static const struct option long_opts[] = {
+      {"prompt", required_argument, NULL, 'p'},
+      {"kb", required_argument, NULL, 'k'},
+      {"help", no_argument, NULL, 'h'},
+      {0, 0, 0, 0},
+  };
 
   int opt;
-  while ((opt = getopt(argc, argv, "p:h")) != -1) {
+  while ((opt = getopt_long(argc, argv, "p:h", long_opts, NULL)) != -1) {
     switch (opt) {
     case 'p':
       prompt = optarg;
+      break;
+    case 'k':
+      if (n_keybindings >= MAX_KEYBINDINGS) {
+        fprintf(stderr, "too many keybindings (max %d)\n", MAX_KEYBINDINGS);
+        return EXIT_FAILURE;
+      }
+      if (!parse_keybinding(optarg, &keybindings[n_keybindings])) {
+        fprintf(stderr, "invalid --kb argument (expected 'CODE:BINDING' with "
+                        "0 <= CODE <= 15)\n");
+        return EXIT_FAILURE;
+      }
+      n_keybindings++;
       break;
     case 'h':
       usage(stdout, argv[0]);
@@ -143,10 +194,21 @@ int main(int argc, char **argv) {
       &error);
   if (!result) {
     g_printerr("SetPrompt failed: %s\n", error->message);
-    g_error_free(error);
     goto cleanup;
   }
   g_variant_unref(result);
+
+  for (size_t i = 0; i < n_keybindings; i++) {
+    result = g_dbus_connection_call_sync(
+        bus, BUS_NAME, OBJECT_PATH, INTERFACE_NAME, "SetKeyBinding",
+        g_variant_new("(is)", keybindings[i].code, keybindings[i].binding),
+        NULL, G_DBUS_CALL_FLAGS_NONE, -1, NULL, &error);
+    if (!result) {
+      g_printerr("SetKeyBinding failed: %s\n", error->message);
+      goto cleanup;
+    }
+    g_variant_unref(result);
+  }
 
   GIOChannel *stdin_channel = g_io_channel_unix_new(STDIN_FILENO);
   g_io_add_watch(stdin_channel, G_IO_IN | G_IO_HUP, on_stdin_ready, &state);
@@ -159,6 +221,7 @@ cleanup:
   g_dbus_connection_signal_unsubscribe(bus, sub_cancelled);
   g_main_loop_unref(state.loop);
   g_object_unref(bus);
+  g_error_free(error);
 
   return state.exit_code;
 }
